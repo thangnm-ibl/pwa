b47f8d4c6ee6f53c1d5a8e8795b842c2c3a75b15
Thang Nguyen <thangnm92@blockchainlabs.asia>
2019-10-04 13:31:35 +0700

    Updates

---
+++ precache-manifest.cc3635f29f9c5a9e144d32a2bb85ac32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb98826e08db0a6f70e9db0fb4051b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4825ceb194c23267c0e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cc0758d87396f0a4c283",
+    "url": "/static/js/2.0b5bb292.chunk.js"
+  },
+  {
+    "revision": "c4825ceb194c23267c0e",
+    "url": "/static/js/main.54a64819.chunk.js"
+  },
+  {
+    "revision": "53c03ac8d19cd8c309c2",
+    "url": "/static/js/runtime-main.96281533.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
